805292f521aab07df3e399ee0d56819fcd1636de
Tsjippy <ewaldha@hotmail.com>
2026-06-05 22:02:19 +0200

    added: shared functionality loader

--- tsjippy-vimeo.php
+++ tsjippy-vimeo.php
@@ -17,7 +17,7 @@ use TSJIPPY;
  * Plugin URI:            https://github.com/Tsjippy/vimeo
  * Tested:                6.9
  * TextDomain:            tsjippy
- * Requires Plugins:    tsjippy-shared-functionality
+ * Requires Plugins:    
  * License: GPLv2 or later
  * License URI: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  *
